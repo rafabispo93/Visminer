18a06f6ce17ff2388b1308bd16a43a2158fa6c33
Sandro Campos <contato@sandrocampos.com>
2016-06-28 17:05:47 -0300

    new vision

--- WebContent/app/technicaldebt/tdanalyzer/tdanalyzer.ctrl.js
+++ WebContent/app/technicaldebt/tdanalyzer/tdanalyzer.ctrl.js
@@ -1,18 +1,79 @@
 homeApp = angular.module('homeApp');
 
 homeApp.controller('TDAnalyzerCtrl', function($scope, $http, $location, $route,
- sidebarService, alertModalService, typeSmellsDetailsService){
+ sidebarService, alertModalService, tdItemModalService, typeSmellsDetailsService){
 	var thisCtrl = this;
-
+	
 	$scope.currentPage = sidebarService.getCurrentPage();
 	$scope.filtered.repository = sidebarService.getRepository();
 	$scope.filtered.tags = sidebarService.getTags();
+	$scope.filtered.commits = sidebarService.getCommits();
 	$scope.filtered.committers = sidebarService.getCommitters();
 	$scope.filtered.debts = sidebarService.getDebts();
 	$scope.selectedTag = $scope.filtered.tags[0];
 	$scope.types = [];
+	$scope.typesAnalized = 0;
 	$scope.currentDesignDebt = null;
 	$scope.currentCodeDebt = null;
+
+	$scope.test = [];
+
+	$scope.data = [];
+
+	$scope.tdItemFiltered = $scope.data;
+
+	$scope.filter = {
+		type: ['Code debt', 'Design debt'],
+		tdItem: ['Duplicated Code', 'Long Method'],
+		isTdItem: ['true', 'false']
+	}
+
+	// Apply filter parameters
+	$scope.filterApply = function() {
+		var tdItemFiltered = [];
+		for (i in $scope.data) {
+			var obj = $scope.data[i];
+			console.log('obj', obj)
+			var accept = 0;
+			var foundType = false;
+			var foundTdItem = false;
+			var foundIsTdItem = false;
+			for (x in obj.debts) {
+				if ($scope.filter.type.indexOf(obj.debts[x].type) > -1) {
+					foundType = true;
+				}
+				if ($scope.filter.tdItem.indexOf(obj.debts[x].name) > -1) {
+					foundTdItem = true;
+				}
+			}
+			if ($scope.filter.isTdItem.indexOf(String(obj.isTdItem)) > -1) {
+				foundIsTdItem = true;
+			}
+			if (foundType && foundTdItem && foundIsTdItem) {
+				tdItemFiltered.push(obj);
+			}
+		}
+		$scope.tdItemFiltered = tdItemFiltered;
+	}
+	
+	$scope.tdItemFormatDate = function(date) {
+	  return moment(date).format('l');
+	}
+
+	// Return the file name
+	$scope.tdItemFormatLocation = function(location) {
+		var loc = location.split('/');
+	  return loc[loc.length-1];
+	}
+
+	$scope.tdItemFormatNotes = function(notes) {
+	  return (notes.length > 20) ? notes.substring(0,17)+'...' : notes;
+	}
+
+	$scope.tdItemEdit = function(tdItem) {
+		tdItemModalService.loadObj(tdItem);
+		$('#tdItemModal').modal('show');
+	}
 
 	thisCtrl.selectView = function(view) {
 		$scope.currentPage = view;
@@ -22,34 +83,96 @@ homeApp.controller('TDAnalyzerCtrl', function($scope, $http, $location, $route,
 	thisCtrl.loadTypes = function(tagId) {
 		$http.get('TypeServlet', {params:{"action": "getAllByTree", "treeId": tagId}})
 		.success(function(data) {
-			console.log('found', data.length, ' types'); 
+			console.log('found', data.length, 'types');
+			console.log('data', data);
+// for (z in data) {
+// 	if(data[z].abstract_types.length > 0) {
+// 		var codesmells = data[z].abstract_types[0].codesmells;
+// 			for (x in codesmells) {
+// 				if (codesmells[x].name == 'Long Method') { 
+// 					// console.log("codesmells", codesmells[x])
+// 					for (j in codesmells[x].methods) {
+// 						console.log('codesmells[x].methods[j].value', codesmells[x].methods[j].value)
+// 						if (codesmells[x].methods[j].value == true) {
+// 							console.log("codesmells.methods[j]", codesmells[x].methods[j])
+// 							throw new Error("achei")
+// 						}
+// 					}
+// 				}
+// 			}
+// 	}
+// }
 			for (var i = 0; i < data.length; i++) {
-				if (data[i].abstract_types[0]) {
-					var hasDebt = thisCtrl.hasDebt(data[i].abstract_types[0].technicaldebts);
-					if (hasDebt) {
-						$scope.types.push(data[i]);				
-					}	
-				}				
+				$scope.types.push(data[i]);
+				var commit = null;
+				for (x in $scope.filtered.commits) {
+					if ($scope.filtered.commits[x]._id == data[i].commit) {
+						commit = $scope.filtered.commits[x];
+						break;
+					}
+				}
+				var debts = thisCtrl.getDebts(data[i]);
+				if (debts.length > 0) {
+					$scope.data.push({
+						"repository": data[i].repository,
+						"commit": commit._id,
+						"identificationDate": new Date(data[i].commit_date.$date),
+						"debts": debts,
+						"metrics": (debts.length > 0) ? data[i].abstract_types[0].metrics : [],
+						"occurredBy": commit.author.name,
+						"location": data[i].file,
+						"isTdItem": false,
+						"principal": "",
+						"interestAmount": "",
+						"newInterestProbability": "",
+						"notes": ""
+					});
+				}
+				$scope.typesAnalized++;
 			}
 		});
 	}
 
-	thisCtrl.hasDebt = function(debtsList) {
-		var hasDebt = false;
-		if (debtsList.length > 0) {
-			for (var j = 0; j < debtsList.length; j++) {
-				if (debtsList[j].name == 'Code Debt' && $.inArray('CODE', $scope.filtered.debts) > -1 && debtsList[j].value) {
-					hasDebt = true;
-				}
-				if (debtsList[j].name == 'Design Debt'  && $.inArray('DESIGN', $scope.filtered.debts) > -1 && debtsList[j].value) {
-					hasDebt = true;
-				}
-			}			
-		}
-		return hasDebt;
-	}
+	
+	thisCtrl.getDebts = function(list) {
+		var debts = [];
 
-	thisCtrl.loadTypes($scope.selectedTag._id);
+		if (typeof list.abstract_types != 'undefined' && list.abstract_types.length > 0) {
+			// looking for long methods
+			if (list.abstract_types[0].codesmells) {
+					for (i in list.abstract_types[0].codesmells) {
+					var codesmells = list.abstract_types[0].codesmells[i];
+					if (codesmells.name == 'Long Method' || codesmells.name == 'Duplicated Code') { 
+						for (j in codesmells.methods) {
+							if (codesmells.methods[j].value == true) {
+								debts.push({
+									type: 'Code Debt',
+									name: codesmells.name,
+									method: codesmells.methods[j].method
+								});
+							}
+						}
+					}
+				}
+			}
+			debtsList = list.abstract_types[0].technicaldebts;
+			if (debtsList.length > 0) {
+				for (var j = 0; j < debtsList.length; j++) {
+					if (debtsList[j].name == 'Code Debt' && $.inArray('CODE', $scope.filtered.debts) > -1 && debtsList[j].value) {
+						debts.push({
+							type: 'Code Debt'
+						});
+					}
+					if (debtsList[j].name == 'Design Debt'  && $.inArray('DESIGN', $scope.filtered.debts) > -1 && debtsList[j].value) {
+						debts.push({
+							type: 'Design Debt'
+						});
+					}
+				}			
+			}
+		}
+		return debts;
+	}
 
 	$scope.loadCurrentDebts = function(type) {
 		var tdList = type.abstract_types[0].technicaldebts;
@@ -110,5 +233,14 @@ homeApp.controller('TDAnalyzerCtrl', function($scope, $http, $location, $route,
 	$scope.showTypeSmellsDetails = function(type) {
 		typeSmellsDetailsService.setType(type);
 		$('#typeSmellsDetails').modal('show');
+	}
+
+	if ($scope.currentPage == 'tdanalyzer') {
+		thisCtrl.loadTypes($scope.selectedTag._id)
+		$('.sidebar-toggle').click();
+		$('#filter-identificationdate').daterangepicker();
+		// $("#tditem-datatable").DataTable({
+		// 	"ordering": false
+		// });
 	}
 });
