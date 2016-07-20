284906c94e9d5b7d7efff9e0366b258e63d55bc9
Sandro Campos <contato@sandrocampos.com>
2016-07-19 23:32:49 -0300

    + tag analysis servlet

---
+++ src/org/visminer/servlets/TagAnalysisServlet.java
@@ -0,0 +1,54 @@
+package org.visminer.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.repositoryminer.persistence.handler.TagAnalysisDocumentHandler;
+
+/**
+ * Servlet implementation class TagAnalysisServlet
+ */
+@WebServlet("/TagAnalysisServlet")
+public class TagAnalysisServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TagAnalysisDocumentHandler tagAnalysisHandler = new TagAnalysisDocumentHandler();
+	private PrintWriter out;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TagAnalysisServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		out = response.getWriter();				
+		String action = request.getParameter("action");
+		
+		switch (action) {
+			case "getAllByTag":
+				getAllByTag(request.getParameter("tagName"));	
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void getAllByTag(String tagName) {
+		List<String> tagAnalysisList = new ArrayList<>();
+		tagAnalysisHandler.getAllByTag(tagName)
+			.forEach(type->tagAnalysisList.add(type.toJson()));
+		out.println(tagAnalysisList);
+	}
+
+}
